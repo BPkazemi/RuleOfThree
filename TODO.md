cb8b001e447cfc355cec84474387aa2762a3c681
Babak Pourkazemi <bp5xj@virginia.edu>
2013-08-14 09:18:38 -0400

    Moving files to localhost, easier to get from git

---
+++ TODO.md
@@ -0,0 +1,5 @@
+1) Refactor to work with Angular
+2) Add back-end capabilities w/ Node&Express and MongoDB
+3) Turn into a chrome web app http://blog.berylliumwork.com/2012/11/turn-angularjs-app-into-google-chrome.html
+4) Find a good looking UI - the background isn't very sleek, and the 
+bordered textareas could be distracting.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 var app = angular.module('app', []);
+
+app.controller('MainCtrl', function($scope) {
+	$scope.validateAndSubmit = function() {
+		$scope.answers = ($scope.firstQ || '') + ' '
+			+ ($scope.secondQ || '') + ' '
+				+ ($scope.thirdQ || '');
+	};
+});
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/',
+		{
+			controller: 'MainCtrl',
+			templateUrl: './partials/partial_introspection.html'
+		})
+	.when('/signup', 
+		{
+			controller: 'MainCtrl',
+			templateUrl: './partials/partial_signup.html'
+		})
+	.otherwise({ redirectTo: '/' });
+});
